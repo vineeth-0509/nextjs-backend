88a3181bec8330de62cabca822f498c52cef80fd
vineeth-0509 <vineeththungani@gmail.com>
2024-06-25 21:50:51 +0530

    added asynchronous component and loading

--- app/page.tsx
+++ app/page.tsx
@@ -1,10 +1,27 @@
-import Image from "next/image";
 
-export default function Home() {
+import axios from 'axios';
+
+async function getUserDetails(){
+  await new Promise((r)=> setTimeout(r,5000))
+// nextjs will render the file loading.tsx until the await operation the asynchronous component executes.
+  const response= await axios.get("https://week-13-offline.kirattechnologies.workers.dev/api/v1/user/details")
+  return response.data;
+}
+
+//asynchronous component
+export default async function Home() {
+  const userDetails= await getUserDetails();
   return (
-    
-    <div>
-      Hi there
+    <div className="flex flex-col justify-center h-screen">
+        <div className="flex justify-center">
+            <div className="border p-8 rounded">
+                <div>
+                    Name: {userDetails?.name}
+                </div>
+                
+                {userDetails?.email}
+            </div>
+        </div>
     </div>
   );
 }
